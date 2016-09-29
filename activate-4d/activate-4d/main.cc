b13cfcd843f8b408a4fac6c68605f876cb8dd8c5
Keisuke Miyako <miyako@wakanda.jp>
2016-09-29 14:14:42 +0900

    1st commit

---
+++ activate-4d/activate-4d/main.cc
@@ -0,0 +1,129 @@
+﻿#include "main.h"
+
+static void usage(void)
+{
+    fprintf(stderr, "Usage: activate-4d [options]\n\n");
+		fprintf(stderr, "Activate the 4D v16 MDI window (class name=XTB_MAIN_MDI_58A2D9EE)\n\n");
+		fprintf(stderr, " -%c path name: %s\n", OPT_PATH , "specify the application full path (default=any)");
+    fprintf(stderr, " -%c class name: %s\n", OPT_CLASS , "specify the class name (default=XTB_MAIN_MDI_58A2D9EE)");
+    fprintf(stderr, " -%c : %s\n", OPT_INFORMATION , "display this help and exit");
+    exit(1);
+}
+
+#ifdef WIN32
+int	opterr = 1;
+int	optind = 1;
+int	optopt;
+wchar_t	*optarg;
+
+int getopt(int argc, OPTARG_T *argv, OPTARG_T opts)
+{
+    static int sp = 1;
+    register int c;
+    register OPTARG_T cp;
+    
+    if(sp == 1)
+        if(optind >= argc ||
+           argv[optind][0] != '-' || argv[optind][1] == '\0')
+            return(EOF);
+        else if(wcscmp(argv[optind], L"--") == NULL) {
+            optind++;
+            return(EOF);
+        }
+    optopt = c = argv[optind][sp];
+    if(c == ':' || (cp=wcschr(opts, c)) == NULL) {
+        ERR(L": illegal option -- ", c);
+        if(argv[optind][++sp] == '\0') {
+            optind++;
+            sp = 1;
+        }
+        return('?');
+    }
+    if(*++cp == ':') {
+        if(argv[optind][sp+1] != '\0')
+            optarg = &argv[optind++][sp+1];
+        else if(++optind >= argc) {
+            ERR(L": option requires an argument -- ", c);
+            sp = 1;
+            return('?');
+        } else
+            optarg = argv[optind++];
+        sp = 1;
+    } else {
+        if(argv[optind][++sp] == '\0') {
+            sp = 1;
+            optind++;
+        }
+        optarg = NULL;
+    }
+    return(c);
+}
+#endif
+
+static void doIt(LPCTSTR arg_class, LPCTSTR arg_path)
+{
+	if(arg_class)
+	{
+		HWND window = NULL;
+		
+		do
+		{
+			window = FindWindowEx(NULL, window, (LPCTSTR)arg_class, NULL);
+			if(window)
+			{
+				if(arg_path)
+				{
+					wchar_t name[1025];
+					GetWindowModuleFileName(window, name, 1024);
+					if(!lstrcmpi(name, arg_path))
+					{
+						break;
+					}
+				}else{
+					break;
+				}
+			}
+		}while(window);
+	
+		ShowWindowAsync(window, SW_RESTORE);
+		//ShowWindow(window, SW_RESTORE);
+		SetWindowPos(window, HWND_TOP, 0, 0, 0, 0,  SWP_NOMOVE | SWP_NOSIZE);
+		//SetActiveWindow(window);
+		SetForegroundWindow(window);
+		AllowSetForegroundWindow(ASFW_ANY);
+		
+	}
+
+	exit(0);
+}
+
+int main(int argc, OPTARG_T argv[])
+{
+	wchar_t *arg_class = L"XTB_MAIN_MDI_58A2D9EE";
+	wchar_t *arg_path = NULL;
+
+	int ch;
+	 
+	OPTARG_T output = NULL;
+			
+	while ((ch = getopt(argc, argv, OPT_LIST)) != -1)
+	{
+		switch (ch)
+		{
+			case OPT_PATH:
+					arg_path = optarg;
+					break;
+			case OPT_CLASS:
+					arg_class = optarg;
+				break;
+			case OPT_INFORMATION:
+			break;
+			default:
+				usage();
+		}
+	} 
+
+	doIt(arg_class, arg_path);
+    
+	return 0;
+}
---
+++ activate-4d/activate-4d/main.h
@@ -0,0 +1,54 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define OPT_PATH					'p'
+#define OPT_CLASS				'c'
+#define OPT_INFORMATION	'h'
+#define OPT_LIST					L"p:c:h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef WIN32
+#include <stdio.h>
+#include <windows.h>
+#define OPTARG_T wchar_t*
+#define main wmain
+#define NULL	0
+#define EOF	(-1)
+#define ERR(s, c)	if(opterr){\
+char errbuf[2];\
+errbuf[0] = c; errbuf[1] = '\n';\
+fputws(argv[0], stderr);\
+fputws(s, stderr);\
+fputwc(c, stderr);}
+#else
+#define OPTARG_T char*
+#endif
+
+#ifdef __GNUC__
+#include <getopt.h>
+#endif
+#ifndef __GNUC__
+
+#ifndef _WINGETOPT_H_
+#define _WINGETOPT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    extern int opterr;
+    extern int optind;
+    extern int optopt;
+    extern OPTARG_T optarg;
+    extern int getopt(int argc, OPTARG_T *argv, OPTARG_T opts);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _GETOPT_H_ */
+#endif  /* __GNUC__ */
+
+#endif
